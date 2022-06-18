a7c856f847ba98b5f05e4ec550fe071b4f6fe265
Raul Ventura <u201714049@upc.edu.pe>
2022-06-18 02:46:54 -0500

    feat: Implemented getting Sources and HeadLines from NewsAPI. Also added providers and Material Modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import {NewsApiService} from "./core/services/news-api.service";
+import {LogoApiService} from "./core/services/logo-api.service";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
@@ -12,9 +21,20 @@ import { HttpClientModule } from "@angular/common/http";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule //   Agregamos esto
+    HttpClientModule, //   Agregamos esto
+
+    //Haciendo referencia a los componentes de Angular Material
+    //que se van a utilizar :
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule  //Listo
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [NewsApiService,LogoApiService], //Aqui van las clases que vas a hacer servicios que se...
+  bootstrap: [AppComponent]                   //...se encuentren disponibles para otros
 })
 export class AppModule { }
--- src/app/core/services/news-api.service.ts
+++ src/app/core/services/news-api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
 
-  constructor() { }
+  apikey='a39a7407bfc7482baaa2c9f1939fe391'; //Creamos esto
+
+  constructor(private http: HttpClient) { //Editamos esto
+  }
+
+  initArticle(){  //Creamos esto
+    return this.getArticlesBySourceId('bbc-news');
+  }
+
+  getSources(){  //Creamos esto
+    return this.http.get(`https://newsapi.org/v2/top-headlines/sources?apiKey=${this.apikey}`)
+  }
+
+  getArticlesBySourceId(sourceId: string){  //Creamos esto
+    return this.http.get(`https://newsapi.org/v2/top-headlines?sources=${sourceId}&apiKey=${this.apikey}`)
+  }
 }
